da290c47bd1e2be0ddd177e7160718d917355a36
Matias Meno <m@tias.me>
2011-10-31 17:36:51 +0100

    Added the index page.

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Formwatcher</title>
+  </head>
+  <body>
+    <h1>Formwatcher</h1>
+    
+    Programming: <a href="http://www.matmeno.com">Matias Meno</a><br />
+
+    Graphics (Imagepicker): Tjandra Mayerhold<br /><br /><br />
+
+
+    <a href="https://github.com/enyo/formwatcher/tags">Get the source at github.com</a>
+    
+    
+    
+    Licenced under the MIT Licence.<br /><br />
+
+    Copyright (c) 2009, Matthias Loitsch<br /><br />
+
+    <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+
+    <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+
+    <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
+
+
+  </body>
+</html>
