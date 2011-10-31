bb64b9030e7687aad54896946dd805d3ceb7dbc5
Matias Meno <m@tias.me>
2011-10-31 18:06:58 +0100

    Added a bit of style.

--- index.php
+++ index.php
@@ -2,18 +2,46 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
+    <style type="text/css">
+      body {
+        font-family: arial, tahoma, sans-serif;
+        font-size: 13px;
+      }
+      #container {
+        width: 600px;
+        margin: 50px auto;
+      }
+      h1, #get-the-source {
+        font-size: 30px;
+        font-family: georgia;
+        font-style: italic;
+      }
+      h1 {
+        margin: 30px 0;
+        font-size: 40px;
+        font-weight: normal;
+      }
+      a {
+        color: blue;
+      }
+    </style>
     <title>Formwatcher</title>
   </head>
   <body>
     <div id="container">
       <h1>Formwatcher</h1>
 
+      <p>The formwatcher is a tool to easily improve forms with javascript based on the jquery library.</p>
+
+      <p>It is designed to be as unobtrusive as possible, so that every form remains intact if javascript is not enabled.</p>
+      
+
       Programming: <a href="http://www.matmeno.com">Matias Meno</a><br />
 
       Graphics (Imagepicker): Tjandra Mayerhold<br /><br /><br />
 
 
-      Get the source at <a href="https://github.com/enyo/formwatcher/tags">github.com</a>.<br /><br /><br />
+      <p id="get-the-source">Get the source at <a href="https://github.com/enyo/formwatcher/tags">github.com</a>.</p><br />
 
 
       <p>Obviously this site is under construction. Hopefully the real site will be online 2011.</p>
