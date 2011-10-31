2f91fab9c1c7da971544beef6a86c995144e5e70
Matias Meno <m@tias.me>
2011-10-31 18:00:30 +0100

    Added opentip.

--- index.php
+++ index.php
@@ -5,27 +5,33 @@
     <title>Formwatcher</title>
   </head>
   <body>
-    <h1>Formwatcher</h1>
-    
-    Programming: <a href="http://www.matmeno.com">Matias Meno</a><br />
+    <div id="container">
+      <h1>Formwatcher</h1>
 
-    Graphics (Imagepicker): Tjandra Mayerhold<br /><br /><br />
+      Programming: <a href="http://www.matmeno.com">Matias Meno</a><br />
+
+      Graphics (Imagepicker): Tjandra Mayerhold<br /><br /><br />
 
 
-    Get the source at <a href="https://github.com/enyo/formwatcher/tags">github.com</a>.<br /><br /><br />
-    
-    
-    
-    Licenced under the MIT Licence.<br /><br />
+      Get the source at <a href="https://github.com/enyo/formwatcher/tags">github.com</a>.<br /><br /><br />
 
-    Copyright (c) 2009, Matthias Loitsch<br /><br />
 
-    <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+      <p>Obviously this site is under construction. Hopefully the real site will be online 2011.</p>
+      <p>You might be interested in another project of ours: <a href="http://www.opentip.org/">www.opentip.org</a>.</p>
 
-    <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+      <br /><br />
 
-    <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
 
+      Licenced under the MIT Licence.<br /><br />
+
+      Copyright (c) 2009, Matthias Loitsch<br /><br />
+
+      <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+
+      <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+
+      <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
+    </div>
 
   </body>
 </html>
